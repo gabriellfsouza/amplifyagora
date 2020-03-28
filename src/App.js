c98c6f525e175069059b6eb27d3fdff8cfeb6fd4
Gabriel Lima F. de Souza <gabriellfsouza@gmail.com>
2020-03-28 18:38:30 -0300

    custom sign methods

--- src/App.js
+++ src/App.js
@@ -1,12 +1,85 @@
 import React from "react";
+import {Auth, Hub} from 'aws-amplify';
+import { Authenticator, AmplifyTheme } from 'aws-amplify-react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+import HomePage from './pages/HomePage';
+import ProfilePage from './pages/ProfilePage';
+import MarketPage from './pages/MarketPage';
+import Navbar from './components/Navbar';
+
 import "./App.css";
 
 class App extends React.Component {
-  state = {};
+  state = {
+    user: null,
+  };
+
+  componentDidMount(){
+    this.getUserData();
+    Hub.listen('auth',this, 'onHubCapsule');
+    console.dir(AmplifyTheme)
+  }
+
+  getUserData = async ()=>{
+    const user = await Auth.currentAuthenticatedUser();
+    user ? this.setState({user}) : this.setState({user:null});
+  }
+
+  onHubCapsule = capsule =>{
+    switch (capsule.payload.event) {
+      case "signIn":
+        console.log('signed in');
+        this.getUserData();
+        break;
+      case "signUp":
+        console.log('signed up');
+        break;
+      case 'signOut':
+        console.log('signed out');
+        this.setState({user:null});
+        break;
+      default:
+        return;
+    }
+  }
+
+  handleSignout = async () =>{
+    try {
+      await Auth.signOut();
+    } catch (error) {
+      console.error('Error siging out user',error);
+    }
+  }
 
   render() {
-    return <div>App</div>;
+    const {user} = this.state;
+    return !user 
+    ?(<Authenticator theme={theme}/>) 
+    :(<Router>
+      <>
+        {/* Navbar */}
+        <Navbar user={user} handleSignout={this.handleSignout} />
+        {/* Routes */}
+        <div className="app-container">
+          <Route exact path="/" component={HomePage} />
+          <Route path="/profile" component={ProfilePage} />
+          <Route path="/markets/:marketId" component={
+            ({match})=>
+              <MarketPage marketId={match.params.marketId} />
+            } />
+        </div>
+      </>
+    </Router>);
   }
 }
+console.log(AmplifyTheme.button);
+const theme = {
+  ...AmplifyTheme,
+  button: {...AmplifyTheme.button, backgroundColor: "var(--amazonOrange)"},
+  sectionBody: {...AmplifyTheme.sectionBody, padding: "5px"},
+  sectionHeader: {...AmplifyTheme.sectionHeader, backgroundColor: "var(--squidInk)"}
+}
 
+// export default withAuthenticator(App, true,[],null,theme);
 export default App;
