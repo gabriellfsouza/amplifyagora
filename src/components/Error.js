3eadf7591bef19bfa0f9536f4dcec6428de711c0
Gabriel Lima F. de Souza <gabriellfsouza@gmail.com>
2020-03-29 07:34:30 -0300

    Easier GraphQL Queries with the Connect Component

--- src/components/Error.js
+++ src/components/Error.js
@@ -1,5 +1,11 @@
 import React from "react";
 
-const Error = () => <div>Error</div>;
+const Error = ({errors}) => (
+  <pre className="error">
+    {errors.map((err,i)=>(
+      <div key={i}>{err.message}</div>
+    ))}
+  </pre>
+);
 
 export default Error;
